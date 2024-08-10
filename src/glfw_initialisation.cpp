5b35f1789383ba6bdf0d2aebe094d444cb39beff
Rany-Touffaha <r_touffaha@hotmail.com>
2024-08-10 13:46:36 +0100

    Create an error callback function for GLFW.

--- src/glfw_initialisation.cpp
+++ src/glfw_initialisation.cpp
@@ -1,13 +1,24 @@
 #include <glfw_initialisation.h>
 #include <GLFW/glfw3.h>
 #include <precomp.h>
+#include <spdlog/spdlog.h>
 
+namespace veng {
 
-veng::GlfwInitialisation::GlfwInitialisation() {
-    if(glfwInit() != GLFW_TRUE) {
-        exit(EXIT_FAILURE);
+    void glfwErrorCallback(std::int32_t errorCode, gsl::czstring message){
+        spdlog::error("Glfw Validation: {}", message);
     }
+
+    veng::GlfwInitialisation::GlfwInitialisation() {
+        glfwSetErrorCallback(glfwErrorCallback);
+
+        if (glfwInit() != GLFW_TRUE) {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    veng::GlfwInitialisation::~GlfwInitialisation() {
+        glfwTerminate();
+    }
+
 }
-
-veng::GlfwInitialisation::~GlfwInitialisation() = default;
-
